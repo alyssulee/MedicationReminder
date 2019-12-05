da498e80ce037835c4f1ee780bc037156d48eac4
Owen Troke-Billard <owen.trokebillard@outlook.com>
2019-12-04 23:51:31 -0700

    Add credentials requirement to every patient api request

--- api/src/main/java/app/Application.java
+++ api/src/main/java/app/Application.java
@@ -1,17 +1,18 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
 import app.controllers.Controller;
 import app.controllers.PatientApiController;
 import model.BloodPressure;
 import model.Patient;
 import model.PatientMeasurement;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
-import static spark.Spark.*;
+import static spark.Spark.before;
+import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class Application {
     public static void main(String[] args) {
--- api/src/main/java/app/controllers/PatientApiController.java
+++ api/src/main/java/app/controllers/PatientApiController.java
@@ -13,9 +13,10 @@ import model.Appointment;
 import model.BloodPressure;
 import model.Doctor;
 import model.Dose;
-import model.LoginCredentials;
 import model.Medication;
+import model.Patient;
 import model.PatientMeasurement;
+import spark.Request;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
@@ -28,14 +29,17 @@ public class PatientApiController implements Controller {
     @Override
     public void registerRoutes() {
 
-        post(basePath + "/verifyLoginAuthenticity", "application/json", (req, res) -> {
-            String json = req.body();
-            LoginCredentials credentials = gson.fromJson(json, LoginCredentials.class);
+        get(basePath + "/verifyLoginAuthenticity", (req, res) -> {
+            Patient patient = getPatientOrThrow(req);
 
-            return credentials;
+            patient.setUsername(null);
+            patient.setPassword(null);
+            return patient;
         }, new JsonTransformer());
 
         get(basePath + "/getUpcomingAppointments", (req, res) -> {
+            getPatientOrThrow(req);
+
             ArrayList<Appointment> appointments = new ArrayList<>();
             appointments.add(new Appointment(UUID.randomUUID(), UUID.randomUUID(), new Date(), new Time(0)));
             appointments.add(new Appointment(UUID.randomUUID(), UUID.randomUUID(), new Date(), new Time(0)));
@@ -43,14 +47,20 @@ public class PatientApiController implements Controller {
         }, new JsonTransformer());
 
         get(basePath + "/getCurrentStreak", (req, res) -> {
+            getPatientOrThrow(req);
+
             return 5;
         }, new JsonTransformer());
 
         get(basePath + "/getLongestStreak", (req, res) -> {
+            getPatientOrThrow(req);
+
             return 30;
         }, new JsonTransformer());
 
         get(basePath + "/getTodaysDoses", (req, res) -> {
+            getPatientOrThrow(req);
+
             ArrayList<Dose> doses = new ArrayList<>();
             doses.add(new Dose(UUID.randomUUID(), new Timestamp(0), 1, "medIdHere1", UUID.randomUUID(), UUID.randomUUID()));
             doses.add(new Dose(UUID.randomUUID(), new Timestamp(0), 2, "medIdHere2", UUID.randomUUID(), UUID.randomUUID()));
@@ -58,6 +68,8 @@ public class PatientApiController implements Controller {
         }, new JsonTransformer());
 
         post(basePath + "/confirmDoseTaken", "application/json", (req, res) -> {
+            getPatientOrThrow(req);
+
             String json = req.body();
             Dose dose = gson.fromJson(json, Dose.class);
             dose.setConfirmerId(UUID.randomUUID());
@@ -65,6 +77,8 @@ public class PatientApiController implements Controller {
         }, new JsonTransformer());
 
         post(basePath + "/markDoseUntaken", "application/json", (req, res) -> {
+            getPatientOrThrow(req);
+
             String json = req.body();
             Dose dose = gson.fromJson(json, Dose.class);
             dose.setConfirmerId(null);
@@ -72,6 +86,8 @@ public class PatientApiController implements Controller {
         }, new JsonTransformer());
 
         get(basePath + "/getAllMeasurements", (req, res) -> {
+            getPatientOrThrow(req);
+
             ArrayList<PatientMeasurement> measurements = new ArrayList<>();
             measurements.add(new PatientMeasurement(new BloodPressure(5, 6), 54, new Timestamp(0)));
             measurements.add(new PatientMeasurement(new BloodPressure(3, 1), 44, new Timestamp(0)));
@@ -80,6 +96,8 @@ public class PatientApiController implements Controller {
 
 
         post(basePath + "/addMeasurement", "application/json", (req, res) -> {
+            getPatientOrThrow(req);
+
             String json = req.body();
             PatientMeasurement measurement = gson.fromJson(json, PatientMeasurement.class);
 
@@ -87,12 +105,16 @@ public class PatientApiController implements Controller {
         }, new JsonTransformer());
 
         get(basePath + "/getDoctor/:doctorId", (req, res) -> {
+            getPatientOrThrow(req);
+
             UUID doctorId = UUID.fromString(req.params("doctorId"));
             return new Doctor(doctorId, "Doc", "Franklin", null, null);
             // Todo: Remember to clear username and password
         }, new JsonTransformer());
 
         get(basePath + "/getMedication/:medId", (req, res) -> {
+            getPatientOrThrow(req);
+
             String medId = req.params("medId");
 
             ArrayList<String> names = new ArrayList<>();
@@ -102,5 +124,15 @@ public class PatientApiController implements Controller {
             return new Medication(medId, names);
         }, new JsonTransformer());
 
+    }
+
+    private Patient getPatientOrThrow(Request request){
+        String username = request.headers("username");
+        String password = request.headers("Password");
+
+        if (username.isEmpty() || password.isEmpty())
+            throw new Error("Username or password was missing.");
+
+        return new Patient(UUID.randomUUID(), "first", "last", username, password);
     }
 }
